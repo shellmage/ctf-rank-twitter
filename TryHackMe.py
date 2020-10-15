9fa5287300f471b168b5777213b4ee4558a20a3b
Charles SENGES <charles.senges.etu@univ-lille.fr>
2020-10-15 12:40:00 +0200

    refacto(TryHackMe): use idiomatic str format syntax

--- TryHackMe.py
+++ TryHackMe.py
@@ -1,6 +1,7 @@
+from lxml import html
+
 import requests
 import json
-from lxml import html
 import Settings
 
 THM_WEBSITE  = 'https://tryhackme.com'
@@ -16,32 +17,28 @@ class TryHackMe:
       self.login()
 
    def login(self):
-      response = self.session.get(THM_WEBSITE + "/login")
+      response = self.session.get(f'{ THM_WEBSITE }/login')
       tree = html.fromstring(response.text)
       csrf_token = tree.xpath('//input[@name="_csrf"]/@value')
-      response = self.session.post(THM_WEBSITE + "/login", data = {"email": self._login, "password": self._password, "_csrf": csrf_token[0]})
+      response = self.session.post(f'{ THM_WEBSITE }/login', data = {'email': self._login, 'password': self._password, '_csrf': csrf_token[0]})
 
    def getTotalUsers(self):
-      response = self.session.get(THM_WEBSITE + "/api/getstats")
+      response = self.session.get(f'{ THM_WEBSITE }/api/getstats')
       globalStats = json.loads(response.text)
 
       return str( globalStats['totalUsers'] )
 
    def getUserRank(self):
-      response = self.session.get(THM_WEBSITE + "/api/usersRank/" + self._username)
+      response = self.session.get(f'{ THM_WEBSITE }/api/usersRank/{ self._username }')
       userStats =  json.loads(response.text)
 
       return str(userStats['userRank'])
 
    def getUserScore(self):
-      response = self.session.get(THM_WEBSITE + "/api/user/" + self._username)
+      response = self.session.get(f'{ THM_WEBSITE }/api/user/{ self._username }')
       userScore =  json.loads(response.text)
 
       return str(userScore['points'])
 
    def pprint(self):
-      return 'TryHackMe rank : {}/{} ({} pts)\n'.format(
-         self.getUserRank(),
-         self.getTotalUsers(),
-         self.getUserScore()
-      )
+      return f'TryHackMe rank : {self.getUserRank()}/{self.getTotalUsers()} ({self.getUserScore()} pts)\n'
